3ffedd62d822dd7cece14de4f29ffdf0e79f610d
badyan <bogdanwov@gmail.com>
2026-06-26 00:37:38 +0300

    correct file structure

--- src/frontend/ui/game_page/main.tsx
+++ src/frontend/ui/game_page/main.tsx
@@ -1,5 +1,5 @@
-import Header from "./header";
-import Chat from "./Chat";
+import Header from "@/ui/header";
+import Chat from "./chat/Chat";
 import Wrapper from "./wrapper";
 
 export default function Game_page() {
