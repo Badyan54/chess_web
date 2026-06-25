04eb254900240de4e7667ac4d2d31c534dc2243f
badyan <bogdanwov@gmail.com>
2026-06-25 23:00:18 +0300

    adjust space code style

--- src/frontend/ui/game_page/header.tsx
+++ src/frontend/ui/game_page/header.tsx
@@ -5,38 +5,40 @@ import Image from "next/image";
 export default function Header() {
   return (
     <>
-        <footer className="flex justify-between px-6 py-4">
+      <footer className="flex justify-between px-6 py-4">
 
-        <div className="flex items-center gap-2">
-          <Image 
-            src={crown} 
-            alt="crown" 
-            width={20} 
-            height={20} 
-            loading="eager"
-          />
-          <h1>Шахи</h1>
-        </div>
-        <div className="flex gap-1 p-px bg-(--light-bg) text-sm rounded-md">
-          <a className="p-1">Правила</a>
-          <a className="p-1">Головна</a>
-          <a className="p-1">Github</a>
-        </div>
-        <div className="flex items-center gap-2">
-          <h2 className="text-white font-bold [text-shadow:_0_0_5px_#96B0A4,_0_0_10px_#96B0A4,_0_0_20px_#96B0A4]">
-            1281u8js81u281
-          </h2>
-          <Image
-            src={id_card} 
-            alt="id-card" 
-            width={20} 
-            height={20} 
-            loading="eager"
-          />
-          {/* user id */}
-        </div>
+      <div className="flex items-center gap-2">
+        <Image 
+          src={crown} 
+          alt="crown" 
+          width={20} 
+          height={20} 
+          loading="eager"
+        />
+        <h1>Шахи</h1>
+      </div>
 
-        </footer>
+      <div className="flex gap-1 p-px bg-(--light-bg) text-sm rounded-md">
+        <a className="p-1">Правила</a>
+        <a className="p-1">Головна</a>
+        <a className="p-1">Github</a>
+      </div>
+      
+      <div className="flex items-center gap-2">
+        <h2 className="text-white font-bold [text-shadow:_0_0_5px_#96B0A4,_0_0_10px_#96B0A4,_0_0_20px_#96B0A4]">
+          1281u8js81u281
+        </h2>
+        <Image
+          src={id_card} 
+          alt="id-card" 
+          width={20} 
+          height={20} 
+          loading="eager"
+        />
+        {/* user id */}
+      </div>
+
+      </footer>
     </>
   );
 }
--- src/frontend/ui/game_page/main.tsx
+++ src/frontend/ui/game_page/main.tsx
@@ -3,7 +3,7 @@ import Header from "./header";
 export default function Game_page() {
   return (
     <>
-        <Header/>
+      <Header/>
     </>
   );
 }
